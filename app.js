bf0ff07ed49b8ddc8ef830252dfec7495676b864
mihai9112 <Mihai@MacBook-Pro.local>
2015-12-15 13:26:23 +0000

    Configuring server to run on port 5000

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-var greeting = 'Hello Dragos';
-console.log(greeting);
+var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.listen(port, function(err){
+    console.log('running server on port ' + port);
+});
