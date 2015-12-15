75cc30df72273223b0c033e0c9425a32f2a00bc6
mihai9112 <Mihai@MacBook-Pro.local>
2015-12-15 15:01:20 +0000

    Added a bootstrap template, configured some static files.

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 
 var port = 3000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
 app.get('/', function(req, res){
     res.send('Hello World');
 });
