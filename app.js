af89ac703c870683f3bcf8d0837a19b9b90d6bc1
mihai9112 <Mihai@MacBook-Pro.local>
2015-12-16 13:39:28 +0000

    Lots of changes, not going to explain. Just configuration stuff

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ var express = require('express');
 
 var app = express();
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-
-app.get('/', function(req, res){
-    res.send('Hello World');
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Independent cleaner',
+        nav: [{
+            Link: '/',
+            Text: 'Admin'
+        }]
+    });
 });
 
-app.listen(port, function(err){
+app.listen(port, function (err) {
     console.log('running server on port ' + port);
 });
